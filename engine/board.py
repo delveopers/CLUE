16e5d14705192b5fa120aad1c4a5089b4a53c949
Shivendra <shivharsh44@gmail.com>
2026-06-25 18:07:36 +0530

    saving progress till now

--- engine/board.py
+++ engine/board.py
@@ -106,3 +106,46 @@ class Board:
       self.black_rook_a_moved = True
     elif piece == -4 and row == 0 and col == 7:
       self.black_rook_b_moved = True
+
+  def long_range_recursion(self, row, col, x_dir, y_dir, team):
+    c_row, c_col = row + x_dir, col + y_dir
+    legal_moves = []
+
+    if c_row < 0 or c_row > 7 or c_col < 0 or c_col > 7:
+      return legal_moves
+
+    c_piece = self.state[c_row][c_col]
+    if c_piece == 0:
+      legal_moves.append((c_row, c_col, None))
+      legal_moves += self.long_range_recursion(c_row, c_col, x_dir, y_dir, team)
+      return legal_moves
+    
+    if team == 1 and c_piece < 0:
+      legal_moves.append((c_row, c_col, None))
+    elif team == -1 and c_piece > 0:
+      legal_moves.append((c_row, c_col, None))
+    return legal_moves
+
+  def king_check(self):
+    white_in_check, black_in_check, white_king_pos, black_king_pos = False, False, None, None
+
+    for row in range(len(self.state)):
+      for col in range(len(self.state[row])):
+        if self.state[row][col] == 6:   ## white king
+          white_king_pos = (row, col)
+          team = 1
+
+          # checking for pawns
+          if row - 1 > 0:
+            if col + 1 < 7 and self.state[row-1][col+1] == -1:
+              white_in_check = True
+            elif col - 1 >= 0 and self.state[row+1][col-1] == -1:
+              white_in_check = True
+          
+          # check for knights
+          knight_moves = ((row + 1, col + 2), (row + 2, col + 1), (row - 1, col - 2), (row - 2, col - 1), (row - 1, col + 2), (row - 2, col + 1), (row + 1, col - 2), (row + 2,col - 1))
+          for i in knight_moves:
+            if i[0] >= 0 and i[1] >= 0 and i[0] <= 7 and i[1] <= 7:
+              r, c = i[0], i[1]
+              if self.state[r][c] == -2:
+                white_in_check = True
